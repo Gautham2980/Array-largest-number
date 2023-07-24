94ad71f88a87376b29db307b4c705190160117ac
Gautham2980 <gautham4816@gmail.com>
2023-07-24 09:44:30 +0530

    arraylargest

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var arr=parseInt(prompt("Enter a array length"));
+var a=[];
+var b=[];
+var temp;
+var n=parseInt(prompt("Enter to find largest number"));
+for(let i=0;i<arr;i++){
+	a[i]=parseInt(prompt("Enter a "+(i+1)+"value"));
+}
+document.write("a="+a+"<br>");
+ 
+for(let i=0;i<arr;i++){
+	for(let j=i+1;j<arr;j++){
+		if(a[i]<a[j]){
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+	}
+}
+b.push(a[i]);
+}
+document.write("Largest Number:"+b[n-1]);
